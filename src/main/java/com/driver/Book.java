9d05058bd032473aee55e11b5d2461fe0884b0f0
shrawani deshpande <shrawanid2000@gmail.com>
2024-01-01 16:30:48 +0530

    constructor changes made

--- src/main/java/com/driver/Book.java
+++ src/main/java/com/driver/Book.java
@@ -14,8 +14,7 @@ public class Book {
 
      }
 
-     public Book(int id, String name, String genre, String author) {
-          this.id = id;
+     public Book(String name, String genre, String author) {
           this.name = name;
           this.genre = genre;
           this.author = author;
